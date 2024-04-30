4f47b1cef3f249eab399138adc848f5a55a7f3ce
Newton Bofulo <newtonombese1@gmail.com>
2024-04-30 16:04:36 +0300

    Successful work on the rectangle

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+import turtle
+
+rectangl = turtle.Turtle()
+
+rectangl.forward(200)
+rectangl.right(90)
+rectangl.forward(100)
+rectangl.right(90)
+rectangl.forward(200)
+rectangl.right(90)
+rectangl.forward(100)
+
+turtle.done()
