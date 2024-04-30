eaae2e5489127ae7220c2fcdb8bc87120a53bc8c
Newton Bofulo <newtonombese1@gmail.com>
2024-04-30 16:04:45 +0300

    Successful work on the octagon

---
+++ octagon.py
@@ -0,0 +1,22 @@
+import turtle
+
+octa = turtle.Turtle()
+
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+octa.right(45)
+octa.forward(100)
+
+
+turtle.done()
