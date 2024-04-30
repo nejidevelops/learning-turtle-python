f8a201e11f3c52180e42c85f18bf11ae3ef2ac26
Newton Bofulo <newtonombese1@gmail.com>
2024-04-30 16:04:25 +0300

    Successful work on the square

---
+++ square.py
@@ -0,0 +1,13 @@
+import turtle
+
+myGuy = turtle.Turtle()
+myGuy.forward(100)
+myGuy.right(90)
+myGuy.forward(100)
+myGuy.right(90)
+myGuy.forward(100)
+myGuy.right(90)
+myGuy.forward(100)
+
+
+turtle.done()
